7907219d2e3b03bdd99b79eecf68023990f82ded
wangyao <1822364545@qq.com>
2025-09-27 10:20:39 +0800

    imguizmo for unreal engine

--- Source/ThirdParty/ImGuiZmoLibrary/Private/ImGuizmo.cpp
+++ Source/ThirdParty/ImGuiZmoLibrary/Private/ImGuizmo.cpp
@@ -835,7 +835,8 @@ namespace IMGUIZMO_NAMESPACE
       const float moy = (1.f - ((io.MousePos.y - position.y) / size.y)) * 2.f - 1.f;
 
       const float zNear = gContext.mReversed ? (1.f - FLT_EPSILON) : 0.f;
-      const float zFar = gContext.mReversed ? 0.f : (1.f - FLT_EPSILON);
+      // const float zFar = gContext.mReversed ? 0.f : (1.f - FLT_EPSILON);
+	  const float zFar = gContext.mReversed ? 0.1f : (1.f - FLT_EPSILON); // for reversed-z, use z = 0.1 for the far plane since the actual far plane may be at infinity
 
       rayOrigin.Transform(makeVect(mox, moy, zNear, 1.f), mViewProjInverse);
       rayOrigin *= 1.f / rayOrigin.w;
@@ -2661,7 +2662,8 @@ namespace IMGUIZMO_NAMESPACE
       // behind camera
       vec_t camSpacePosition;
       camSpacePosition.TransformPoint(makeVect(0.f, 0.f, 0.f), gContext.mMVP);
-      if (!gContext.mIsOrthographic && camSpacePosition.z < 0.001f && !gContext.mbUsing)
+      // if (!gContext.mIsOrthographic && camSpacePosition.z < 0.001f && !gContext.mbUsing)
+	  if (!gContext.mIsOrthographic && camSpacePosition.z > 10.0f && !gContext.mbUsing)
       {
          return false;
       }
